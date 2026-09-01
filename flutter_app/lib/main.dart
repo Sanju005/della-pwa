5dd44259aa0620a2beb956053a8c31f8d2649e7f
Sanju <mrsanju005@gmail.com>
2026-09-01 18:14:28 +0800

    save

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/widgets.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app.dart';
 import 'core/config/app_config.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await initializeDateFormatting('en_MY');
+
   await Supabase.initialize(
     url: AppConfig.supabaseUrl,
     publishableKey: AppConfig.supabaseAnonKey,
   );
+
   runApp(const SwiperApp());
 }
